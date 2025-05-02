eb734bd7207b2930631780824d47cb25043e2498
Michael <michaellunn@git.edstem.org>
2025-05-02 22:22:08 +1000

    Added homeview added to gitignore

---
+++ Roomies/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Roomies
+//
+//  Created by Michael on 2/5/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
